f4392e120f496da63de80c363a488c5154175f9c
Daniel  Choo <danielchoo@Daniels-MacBook-Pro.local>
2018-08-01 20:36:39 +0800

    Corrected email text bug

--- sendEmail.py
+++ sendEmail.py
@@ -14,7 +14,12 @@ def send_email(id):
     N = logs.find_one({"StudentId": id})["Occurence"]
     #print(json.dumps(id))
     f = students.find_one({"card_number": id})
-    text = "Dear " + f["form_teacher_one"] + " and " + f["form_teacher_two"] + ",\n\nThis is an email to inform you that " + f["name"] + " from class " + f["class"] + " has left school early on " + now.strftime("%d-%m-%Y") + " at " + now.strftime("%H:%M") + ".\n\nHe has left school early " + str(N) + " times.\n\nThank you."
+    text = "Dear " + f["form_teacher_one"] + " and " + f["form_teacher_two"] + ",\n\nThis is an email to inform you that " + f["name"] + " from class " + f["class"] + " has left school early on " + now.strftime("%d-%m-%Y") + " at " + now.strftime("%H:%M") + ".\n\n"
+    if(N != 1):
+        text += "He has left school early " + str(N) + " times."
+    else:
+        text += "He has only left school early once."
+    text += "\n\nThank you."
     email(f['teachers_emails'],text,f["name"])
 
 
